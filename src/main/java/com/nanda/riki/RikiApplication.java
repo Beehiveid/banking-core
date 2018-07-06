c732b584719d712b8e50b28e7e8af8ec9d1e91af
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2018-07-06 21:21:52 +0700

    Initial Commit

---
+++ src/main/java/com/nanda/riki/RikiApplication.java
@@ -0,0 +1,12 @@
+package com.nanda.riki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RikiApplication.class, args);
+	}
+}
